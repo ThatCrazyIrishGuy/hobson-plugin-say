eed66d590c81515f3ac870a155f3e2b078ff3609
Darren Britton <darren.britton@sap.com>
2017-05-04 16:52:44 +0100

    added config usage

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ exports = module.exports;
 const jsonfile = require('jsonfile');
 const path = require('path');
 
+const config = jsonfile.readFileSync(path.resolve(__dirname, 'config.json'));
+
 exports.say = {
     this: function(values, callback) {
         var response = values.join(' ');
         callback(response);
     },
     lexify: {
-        match: 'say', //any lexical pos tag
+        match: config.extractAfter, //any lexical pos tag
         extract: 'after' // match, before, after
     }
 };
