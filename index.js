d9a3bcd5ca778978512d3bd4f8cca07895dffa48
Darren Britton <darrenbritton@hotmail.com>
2017-11-10 23:29:25 +0000

    consume hobson-response-types

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 exports = module.exports;
+const hobsonResponseTypes = require('hobson-response-types');
 const jsonfile = require('jsonfile');
 const path = require('path');
 
 const config = jsonfile.readFileSync(path.resolve(__dirname, 'config.json'));
 
 exports.say = {
-    this: function(values, callback) {
+    this: {
+      type: hobsonResponseTypes.text,
+      execute: function(values, callback) {
         var response = values.join(' ');
         callback(response);
+      }
     },
     lexify: {
         match: config.extractAfter, //any lexical pos tag
